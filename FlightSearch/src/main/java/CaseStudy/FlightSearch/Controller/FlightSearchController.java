343ff75d1c422cabb4e963709a1cc1e979c498de
Sultan Qureshi <sultanmq1999@gmail.com>
2021-12-02 22:49:09 +0530

    FlightSearch

---
+++ FlightSearch/src/main/java/CaseStudy/FlightSearch/Controller/FlightSearchController.java
@@ -0,0 +1,47 @@
+package CaseStudy.FlightSearch.Controller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import CaseStudy.FlightSearch.Repository.FlightRepository;
+import CaseStudy.FlightSearch.Model.Flight;
+
+@RestController
+public class FlightSearchController {
+	
+	@Autowired
+	private FlightRepository flightRepository;
+	
+	@GetMapping("/findallflights")
+	public List<Flight> getAllFlights(){
+		return flightRepository.findAll();
+	}
+	
+	@GetMapping("/find/{origin}/{destination}/{departureDate}")
+	public List<Flight> getFlights(@PathVariable String origin,@PathVariable String destination,@PathVariable String departureDate)
+	{
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		formatter = formatter.withLocale(Locale.ENGLISH);
+		LocalDate dpDate = LocalDate.parse(departureDate, formatter);
+
+		return flightRepository.findByDetails(origin,destination,dpDate);
+	}
+	
+	//Not a part of this ............
+	@PostMapping("/addFlight")
+	public String saveFlight(@RequestBody Flight flight) {
+
+		flightRepository.save(flight);
+		return "Added Flight :"+flight.get_id()+" Number: "+flight.getFlightNo();
+	}
+
+}
---
+++ FlightSearch/src/main/java/CaseStudy/FlightSearch/Model/Flight.java
@@ -0,0 +1,136 @@
+package CaseStudy.FlightSearch.Model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Document(collection= "Flight")
+public class Flight {
+	
+	@Id
+	private String _id; 	 
+	@Field
+	private String FlightNo;
+	@Field
+	private String Airline;
+	@Field
+	private String Origin;
+	@Field
+	private String Destination;
+	@Field
+	private LocalDate DepartureDate;
+	@Field
+	private LocalDateTime DepartureDateAndTime;
+	@Field
+	private LocalDateTime ArrivalDateAndTime;
+	@Field
+	private double EconomyClassPrice;
+	@Field
+	private double BusinessClassPrice;
+
+
+
+	public String get_id() {
+		return _id;
+	}
+
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+
+	public String getFlightNo() {
+		return FlightNo;
+	}
+
+
+	public void setFlightNo(String flightNo) {
+		FlightNo = flightNo;
+	}
+
+
+	public String getAirline() {
+		return Airline;
+	}
+
+
+	public void setAirline(String airline) {
+		Airline = airline;
+	}
+
+
+	public String getOrigin() {
+		return Origin;
+	}
+
+
+	public void setOrigin(String origin) {
+		Origin = origin;
+	}
+
+
+	public String getDestination() {
+		return Destination;
+	}
+
+
+	public void setDestination(String destination) {
+		Destination = destination;
+	}
+
+
+	public LocalDateTime getDepartureDateAndTime() {
+		return DepartureDateAndTime;
+	}
+
+
+	public void setDepartureDateAndTime(LocalDateTime departureDateAndTime) {
+		DepartureDateAndTime = departureDateAndTime;
+	}
+
+
+	public LocalDate getDepartureDate() {
+		return DepartureDate;
+	}
+
+
+	public void setDepartureDate(LocalDate departureDate) {
+		
+		DepartureDate = departureDate;
+	}
+
+
+	public LocalDateTime getArrivalDateAndTime() {
+		return ArrivalDateAndTime;
+	}
+
+
+	public void setArrivalDateAndTime(LocalDateTime arrivalDateAndTime) {
+		ArrivalDateAndTime = arrivalDateAndTime;
+	}
+
+
+	public double getEconomyClassPrice() {
+		return EconomyClassPrice;
+	}
+
+
+	public void setEconomyClassPrice(double economyClassPrice) {
+		EconomyClassPrice = economyClassPrice;
+	}
+
+
+	public double getBusinessClassPrice() {
+		return BusinessClassPrice;
+	}
+
+
+	public void setBusinessClassPrice(double businessClassPrice) {
+		BusinessClassPrice = businessClassPrice;
+	}
+}
