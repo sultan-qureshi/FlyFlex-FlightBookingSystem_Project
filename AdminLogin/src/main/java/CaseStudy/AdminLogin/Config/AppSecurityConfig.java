05e0815c06f49ba939532eed98910f680ed09c3a
Sultan Qureshi <sultanmq1999@gmail.com>
2021-12-05 11:51:04 +0530

    updated

--- AdminLogin/src/main/java/CaseStudy/AdminLogin/Config/AppSecurityConfig.java
+++ AdminLogin/src/main/java/CaseStudy/AdminLogin/Config/AppSecurityConfig.java
@@ -6,14 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
+
+
 	@Autowired
 	private UserDetailsService userDetailsService;
 
@@ -24,5 +28,14 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 		return provider;
 		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests().antMatchers("/","admin","admin/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin();
 	}
 }
