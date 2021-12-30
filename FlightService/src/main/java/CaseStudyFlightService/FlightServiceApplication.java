0c86461c0a69c222f7ee11e65e5c9853e097ea43
Sultan Qureshi <sultanmq1999@gmail.com>
2021-12-31 02:27:09 +0530

    Changed

--- FlightService/src/main/java/CaseStudyFlightService/FlightServiceApplication.java
+++ FlightService/src/main/java/CaseStudyFlightService/FlightServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
 @EnableEurekaClient
 public class FlightServiceApplication {
